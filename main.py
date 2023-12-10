e90d7ab1b4f2d2d6ecc18a90e8ff19ef0e17e002
sudoskys <coldlando@hotmail.com>
2023-12-10 15:40:43 +0800

    :rocket: chore: Update project dependencies and configuration files

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 from dotenv import load_dotenv
 from loguru import logger
 
+from app.controller import BotRunner
 from app_conf import settings
 
 load_dotenv()
@@ -13,22 +14,19 @@ logger.remove()
 print("从配置文件中读取到的DEBUG为", settings.app.debug)
 handler_id = logger.add(sys.stderr, level="INFO" if not settings.app.debug else "DEBUG")
 # 添加文件写出
-logger.add(sink='run.log',
-           format="{time} - {level} - {message}",
-           level="INFO",
-           rotation="100 MB",
-           enqueue=True
-           )
-
-from app.controller import BotRunner
+logger.add(
+    sink="run.log",
+    format="{time} - {level} - {message}",
+    level="INFO",
+    rotation="100 MB",
+    enqueue=True,
+)
 
 logger.info("Log Is Secret, Please Don't Share It To Others")
 
 
 async def main():
-    await asyncio.gather(
-        BotRunner().run()
-    )
+    await asyncio.gather(BotRunner().run())
 
 
 loop = asyncio.get_event_loop()
